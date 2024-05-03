03adc7a528324b762a528484002aeb1a79679876
oleg.gladkov <sm00th@rambler.ru>
2024-05-03 12:14:45 +0800

    changed main return

--- main.py
+++ main.py
@@ -11,13 +11,13 @@ class FaceSwapper:
         self.cfg = OmegaConf.load(config_path)
         download_ckpts(self.cfg)
 
-    def execute(self, input_image_path: str, source_image_path: str) -> Image.Image:
+    def do_swap(self, input_image_path: str, source_image_path: str) -> Image.Image:
         script = FaceSwapScript()
         source_img = Image.open(source_image_path)
         input_img = Image.open(input_image_path)
         swapped = script.process(source_img=source_img, input_img=input_img, cfg=self.cfg)
 
-        swapped.show()
+        return swapped
 
 if __name__ == '__main__':
     source_image_path = '/home/sm00th/Projects/upwork/dayo/src/woman_6_1024.jpg'
